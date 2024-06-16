1c591c91430269a0664d68eafffd5dc9621aebf5
Hans Jørgen Lachmann <hans@dynapro.no>
2024-06-16 13:07:13 +0200

    navar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,8 @@ const Navbar = () => {
   return (
     <div id="navbar-container">
       <div id="navbar">
-        <img src="assets/radix-logo-dark.png" alt="dev mode setup" />
-        <img src="assets/developer-img.png" alt="dev mode setup" />
+        <img src="https://github.com/hansjlachmann/react-ts-dapp/blob/main/src/assets/radix-logo-dark.png" alt="dev mode setup" />
+        <img src="https://github.com/hansjlachmann/react-ts-dapp/blob/main/src/assets/developer-img.png" alt="dev mode setup" />
       </div>
 
       <div id="connect-btn">
