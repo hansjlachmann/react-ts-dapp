943a36c25ed5bcf395fecf2474ef0474591ca7c0
Hans Jørgen Lachmann <hans@dynapro.no>
2024-06-16 13:05:21 +0200

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,8 @@ const Navbar = () => {
   return (
     <div id="navbar-container">
       <div id="navbar">
-        <img src="src/assets/radix-logo-dark.png" alt="dev mode setup" />
-        <img src="src/assets/developer-img.png" alt="dev mode setup" />
+        <img src="assets/radix-logo-dark.png" alt="dev mode setup" />
+        <img src="assets/developer-img.png" alt="dev mode setup" />
       </div>
 
       <div id="connect-btn">
